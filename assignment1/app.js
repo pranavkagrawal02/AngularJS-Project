dbbf0f353c410dd7e6c9635042a35730ef264c3e
pranav <pranavkumaragrawal02@gmail.com>
2024-10-30 15:04:06 +0530

    Lec12

--- assignment1/app.js
+++ assignment1/app.js
@@ -2,24 +2,34 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+.controller('LunchCheckController', function ($scope) {
+    $scope.name = "Pranav"
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  $scope.checkLunch = function () {
-    if ($scope.lunchMenu === undefined || $scope.lunchMenu.trim() === "") {
-      $scope.message = "Please enter data first";
-    } else {
-      var items = $scope.lunchMenu.split(',');
-      var numItems = items.filter(item => item.trim() !== "").length;
 
-      if (numItems <= 3) {
-        $scope.message = "Enjoy!";
+    $scope.checkLunch = function () {
+
+
+
+      if  
+   ($scope.lunchMenu === undefined || $scope.lunchMenu.trim() === "") {
+        $scope.message = "Please enter data first";
       } else {
-        $scope.message = "Too much!";
+        var items = $scope.lunchMenu.split(',');
+        var numItems = items.filter(item => item.trim() !== "").length;
+
+        if (numItems <= 3) {
+          $scope.message = "Enjoy!";
+        } else {
+          $scope.message = "Too much!";
+        }
       }
-    }
-  };
-}
+
+
+      //alert("Hello");
+    };
+
+
+});
+
 
 })();
